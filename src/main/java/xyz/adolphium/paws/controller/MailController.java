542cdaa2620e4924bb4c5d8a5e566e8c3d8d3f6e
Patrick Täsler <patrick.taesler.biz@gmail.com>
2024-01-28 13:15:21 +0100

    MailController validated request

--- src/main/java/xyz/adolphium/paws/controller/MailController.java
+++ src/main/java/xyz/adolphium/paws/controller/MailController.java
@@ -1,8 +1,10 @@
 package xyz.adolphium.paws.controller;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +13,14 @@ import xyz.adolphium.paws.service.MailService;
 
 @RequiredArgsConstructor
 @RestController
+@Validated
 public class MailController {
 
 
     private final MailService mailService;
 
     @PostMapping("/notification")
-    ResponseEntity<Void> sendEmail(@RequestBody @NotNull EmailSendRequest request) {
+    ResponseEntity<Void> sendEmail(@RequestBody @NotNull @Valid  EmailSendRequest request) {
         mailService.sendMailTo(request.email(), request.subject(), request.text());
         return ResponseEntity.accepted()
                 .build();
